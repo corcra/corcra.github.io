cd0fd89446f6aa6407445aea39155c5eabd9d660
corcra <corcra@users.noreply.github.com>
2025-01-11 13:07:19 +0000

    Deploying to gh-pages from @ corcra/corcra.github.io@0f1f2355fc6c1a9b1546ba1ab856fdae4368ad14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%74%65%70%68.%68%79%6C%61%6E%64@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
